693af1abe641ed2fdc0b501aab6298ba4a43b83b
daolao <dalao1002@gmail.com>
2019-02-12 09:39:59 +0100

    View Entity

--- src/main/java/santatecla/itinerarios/model/View.java
+++ src/main/java/santatecla/itinerarios/model/View.java
@@ -1,4 +1,21 @@
 package santatecla.itinerarios.model;
 
+import lombok.Data;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
+@Data
 public class View {
+    @Id
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn
+    private Set<Form> forms;
 }
