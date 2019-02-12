cb4e326fa3267bfcee20a57e8640c8f06270c036
daolao <dalao1002@gmail.com>
2019-02-12 09:55:36 +0100

    Unit Model, Unit repo, MustacheController

--- src/main/java/santatecla/itinerarios/model/Itinerary.java
+++ src/main/java/santatecla/itinerarios/model/Itinerary.java
@@ -1,4 +1,19 @@
 package santatecla.itinerarios.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
 public class Itinerary {
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn
+    private Unit unit;
 }
--- src/main/java/santatecla/itinerarios/model/Unit.java
+++ src/main/java/santatecla/itinerarios/model/Unit.java
@@ -1,4 +1,27 @@
 package santatecla.itinerarios.model;
 
+import lombok.Data;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
+@Data
 public class Unit {
+    @Id
+    @Column(length = 30)
+    private String title;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn
+    private Set<Itinerary> itineraries;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn
+    private Set<Form> forms;
 }
