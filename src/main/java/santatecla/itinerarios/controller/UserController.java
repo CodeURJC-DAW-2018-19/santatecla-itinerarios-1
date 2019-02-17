fc16c125f7c281d0a9a99f9360192afb0d3d3b36
daolao <dalao1002@gmail.com>
2019-02-17 18:49:31 +0100

    register user

--- src/main/java/santatecla/itinerarios/controller/UserController.java
+++ src/main/java/santatecla/itinerarios/controller/UserController.java
@@ -1,15 +1,14 @@
 package santatecla.itinerarios.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import santatecla.itinerarios.model.User;
 import santatecla.itinerarios.repo.UserRepository;
 
 import javax.validation.Valid;
 
-@RestController
+@Controller
 public class UserController {
     private UserRepository repo;
 
@@ -18,7 +17,7 @@ public class UserController {
     }
 
     @PostMapping("/signUp")
-    public String signUp(@Valid @RequestBody User user) {
+    public String signUp(@Valid @ModelAttribute("user") User user) {
         this.repo.save(user);
         return "login";
     }
