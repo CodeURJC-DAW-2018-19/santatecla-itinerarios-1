e4f5947684cecb208aac658b00c75440f541410f
daolao <dalao1002@gmail.com>
2019-02-24 00:50:15 +0100

    add unit

--- src/main/java/santatecla/itinerarios/controller/UnitController.java
+++ src/main/java/santatecla/itinerarios/controller/UnitController.java
@@ -2,9 +2,9 @@ package santatecla.itinerarios.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import santatecla.itinerarios.model.Unit;
@@ -21,7 +21,7 @@ public class UnitController {
     }
 
     @PostMapping
-    public void addUnit(@RequestBody Unit unit) {
+    public void addUnit(@ModelAttribute Unit unit) {
         this.repository.save(unit);
     }
 
