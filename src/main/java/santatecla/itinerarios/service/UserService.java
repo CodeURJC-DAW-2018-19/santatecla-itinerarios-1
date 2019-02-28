c0b81a4f6fd2c5572bc051342dc9a43bf1ddea38
daolao <dalao1002@gmail.com>
2019-02-28 18:48:43 +0100

    getCurrentUser

--- src/main/java/santatecla/itinerarios/service/UserService.java
+++ src/main/java/santatecla/itinerarios/service/UserService.java
@@ -1,10 +1,14 @@
 package santatecla.itinerarios.service;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import santatecla.itinerarios.model.User;
 import santatecla.itinerarios.repo.UserRepository;
+
+import java.security.Principal;
 
 import static org.springframework.security.core.userdetails.User.withUsername;
 
@@ -19,8 +23,16 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return this.repo.findByUsername(username)
-                .map(u -> withUsername(u.getUsername()).password(u.getPassword()).roles(u.getRoles().toArray(new String[0])).build())
-                .orElseThrow(() -> new UsernameNotFoundException(username));
+        final User user = this.read(username);
+        return withUsername(user.getUsername()).password(user.getPassword()).roles(user.getRoles().toArray(new String[0])).build();
+    }
+
+    public User read(String username) {
+        return this.repo.findById(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+    public User getCurrentUser() {
+        Principal principal = SecurityContextHolder.getContext().getAuthentication();
+        return this.read(principal.getName());
     }
 }
