3be1a88f6532f6884e27475240b7dcc9a3b2ebb0
daolao <dalao1002@gmail.com>
2019-03-04 11:57:13 +0100

    delete form api

--- src/main/java/santatecla/itinerarios/controller/FormController.java
+++ src/main/java/santatecla/itinerarios/controller/FormController.java
@@ -1,8 +1,6 @@
 package santatecla.itinerarios.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
