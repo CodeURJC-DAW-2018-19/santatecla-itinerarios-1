778b46eb2b6c8eb839ed8056b5d61eae33d0aae5
daolao <dalao1002@gmail.com>
2019-03-11 11:01:30 +0100

    upload image with api rest

--- src/main/java/santatecla/itinerarios/security/WebSecurityConfig.java
+++ src/main/java/santatecla/itinerarios/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import santatecla.itinerarios.service.UserService;
 
 @Configuration
@@ -38,6 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().httpBasic();
         
         http.requiresChannel().anyRequest().requiresSecure();
+        
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
     @Override
