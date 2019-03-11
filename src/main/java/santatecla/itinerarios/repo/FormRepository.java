4ba1b59bbff392ba3eec5fa58e7bb633a79e1946
daolao <dalao1002@gmail.com>
2019-03-11 09:49:03 +0100

    resolve path conflict

--- src/main/java/santatecla/itinerarios/repo/FormRepository.java
+++ src/main/java/santatecla/itinerarios/repo/FormRepository.java
@@ -3,6 +3,7 @@ package santatecla.itinerarios.repo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.data.web.PageableDefault;
 import santatecla.itinerarios.model.Form;
 
@@ -17,5 +18,6 @@ public interface FormRepository extends JpaRepository<Form, Long> {
         String getTitle();
     }
 
+    @RestResource(exported = false)
     Set<Basic> findAllByUnit_Id(Long id);
 }
