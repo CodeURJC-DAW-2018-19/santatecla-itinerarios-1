74811d742ccd6083e82cad5df2fcb7891bcc30bf
daolao <dalao1002@gmail.com>
2019-03-18 22:36:49 +0100

    hide user

--- src/main/java/santatecla/itinerarios/repo/UserRepository.java
+++ src/main/java/santatecla/itinerarios/repo/UserRepository.java
@@ -1,10 +1,12 @@
 package santatecla.itinerarios.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import santatecla.itinerarios.model.User;
 
 import java.util.Optional;
 
+@RepositoryRestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsername(String username);
 
