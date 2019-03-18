0cce6b8381adbce90118fa0844535b69cf3ef608
daolao <dalao1002@gmail.com>
2019-03-18 02:01:14 +0100

    docker

--- docker/create_image.bat
+++ docker/create_image.bat
@@ -1,4 +1,7 @@
-.\gradlew bootJar
+call .\gradlew bootJar
 copy .\build\libs\santatecla.itinerarios-0.1.0.jar .\docker\app.jar
 cd docker
-docker-compose up
+call docker-machine start default
+call docker build --tag=daw .
+call docker tag daw hellodalao/daw:fase3
+call docker push hellodalao/daw:fase3
--- docker/create_image.sh
+++ docker/create_image.sh
@@ -2,4 +2,7 @@
 .\gradlew bootJar
 cp .\build\libs\santatecla.itinerarios-0.1.0.jar .\docker\app.jar
 cd docker
-docker-compose up
+docker-machine start default
+docker build --tag=daw .
+docker tag daw hellodalao/daw:fase3
+docker push hellodalao/daw:fase3
