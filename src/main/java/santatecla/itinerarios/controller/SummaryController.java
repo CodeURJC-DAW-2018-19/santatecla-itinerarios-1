826b238fa516c437ca6824a63652df365786344a
daolao <dalao1002@gmail.com>
2019-03-30 18:57:09 +0100

    remove itineraries summary endpoint

--- src/main/java/santatecla/itinerarios/controller/SummaryController.java
+++ src/main/java/santatecla/itinerarios/controller/SummaryController.java
@@ -1,27 +1,29 @@
 package santatecla.itinerarios.controller;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonView;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import santatecla.itinerarios.model.SummaryView;
 import santatecla.itinerarios.model.Unit;
 import santatecla.itinerarios.repo.UnitRepository;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/summary")
 public class SummaryController {
-    private UnitRepository repository;
+    private UnitRepository unitRepository;
 
     public SummaryController(UnitRepository repository) {
-        this.repository = repository;
+        this.unitRepository = repository;
     }
 
     @JsonView(SummaryView.class)
     @GetMapping("units")
     public List<Unit> units() {
-        return this.repository.findAll();
+        return this.unitRepository.findAll();
     }
 }
