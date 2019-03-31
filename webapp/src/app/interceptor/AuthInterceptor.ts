21f5aa1cebf01a112f4bf8dd4fb2356c1d40e5dd
daolao <dalao1002@gmail.com>
2019-03-31 16:05:54 +0200

    fix interceptor logout on failed

--- webapp/src/app/interceptor/AuthInterceptor.ts
+++ webapp/src/app/interceptor/AuthInterceptor.ts
@@ -1,23 +1,23 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable, throwError} from "rxjs";
-import {CredentialService} from "../service/credential.service";
-import {catchError} from "rxjs/operators";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { CredentialService } from '../service/credential.service';
+import { catchError } from 'rxjs/operators';
 
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private manager: CredentialService) {
-  }
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (this.manager.hasCredential()) {
-      const auth = req.clone({
-        headers: req.headers.set('Authorization', this.manager.credential)
-      });
-      return next.handle(auth).pipe(catchError(err => {
-        this.manager.invalidate();
-        return throwError(err);
-      }));
-    } else {
-      return next.handle(req);
+    constructor(private manager: CredentialService) {
     }
-  }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (this.manager.hasCredential()) {
+            const auth = req.clone({
+                headers: req.headers.set('Authorization', this.manager.credential)
+            });
+            return next.handle(auth).pipe(catchError(err => {
+                if (err.status === 401) { this.manager.invalidate(); }
+                return throwError(err);
+            }));
+        } else {
+            return next.handle(req);
+        }
+    }
 }
