52de07970f5b829bccb04b9341c9eefc3d626382
daolao <dalao1002@gmail.com>
2019-03-31 16:10:09 +0200

    change quotes

--- webapp/src/app/service/authentication.service.ts
+++ webapp/src/app/service/authentication.service.ts
@@ -1,48 +1,48 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {CredentialService} from "./credential.service";
-import {CredentialDTO} from "../model/credential-dto";
-import {API_LOG_OUT, API_AUTH, API_SIGN_UP} from "../config";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CredentialService } from './credential.service';
+import { CredentialDTO } from '../model/credential-dto';
+import { API_LOG_OUT, API_AUTH, API_SIGN_UP } from '../config';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthenticationService {
-  private roles: string[];
+    private roles: string[];
 
-  constructor(private http: HttpClient, private credential: CredentialService) {
-  }
-
-  hasRole(role: string) {
-    if (this.roles != null) {
-      return this.roles.includes(role)
+    constructor(private http: HttpClient, private credential: CredentialService) {
     }
-    return false;
-  }
 
-  private authenticate(callback?: () => void, error?: () => void): void {
-    this.http.get<{ roles }>(API_AUTH).subscribe(auth => {
-      this.credential.save();
-      this.roles = auth.roles;
-      return callback && callback();
-    }, error);
-  }
+    hasRole(role: string) {
+        if (this.roles != null) {
+            return this.roles.includes(role)
+        }
+        return false;
+    }
 
-  login(credential: CredentialDTO, callback?: () => void, error?: () => void): void {
-    this.credential.update(credential);
-    this.authenticate(callback, error);
-  }
+    private authenticate(callback?: () => void, error?: () => void): void {
+        this.http.get<{ roles }>(API_AUTH).subscribe(auth => {
+            this.credential.save();
+            this.roles = auth.roles;
+            return callback && callback();
+        }, error);
+    }
 
-  register(credential: CredentialDTO, callback?: () => void): void {
-    this.http.post(API_SIGN_UP, credential).subscribe(r => this.login(credential, callback));
-  }
+    login(credential: CredentialDTO, callback?: () => void, error?: () => void): void {
+        this.credential.update(credential);
+        this.authenticate(callback, error);
+    }
 
-  get authenticated(): boolean {
-    return this.credential.hasCredential();
-  }
+    register(credential: CredentialDTO, callback?: () => void): void {
+        this.http.post(API_SIGN_UP, credential).subscribe(r => this.login(credential, callback));
+    }
 
-  logout() {
-    this.credential.invalidate();
-    this.http.post(API_LOG_OUT, null).subscribe();
-  }
+    get authenticated(): boolean {
+        return this.credential.hasCredential();
+    }
+
+    logout() {
+        this.credential.invalidate();
+        this.http.post(API_LOG_OUT, null).subscribe();
+    }
 }
