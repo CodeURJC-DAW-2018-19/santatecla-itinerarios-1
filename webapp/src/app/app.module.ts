22cfa42bd402db3880253d2adfb996a372bd789c
daolao <dalao1002@gmail.com>
2019-04-02 22:24:54 +0200

    sort import

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,159 +1,166 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {AuthInterceptor} from "./interceptor/AuthInterceptor";
-import {UnitsComponent} from './units/units.component';
-import {ViewComponent} from './view/view.component';
-import {FileComponent} from './file/file.component';
-import {FilesComponent} from './files/files.component';
-import {LoginComponent} from './login/login.component';
-import {SignUpComponent} from './signUp/signUp.component';
-import {ItineraryComponent} from './itinerary/itinerary.component';
-import {UnitComponent} from './unit/unit.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CovalentDynamicFormsModule} from "@covalent/dynamic-forms";
-import {CovalentMarkdownModule} from "@covalent/markdown";
-import {CovalentHighlightModule} from "@covalent/highlight";
-import {CovalentHttpModule} from "@covalent/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AuthInterceptor } from './interceptor/AuthInterceptor';
+import { CredentialService } from './service/credential.service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UnitsComponent } from './units/units.component';
+import { ViewComponent } from './view/view.component';
+import { FileComponent } from './file/file.component';
+import { FilesComponent } from './files/files.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './signUp/signUp.component';
+import { ItineraryComponent } from './itinerary/itinerary.component';
+import { UnitComponent } from './unit/unit.component';
+import { ErrorComponent } from './error/error.component';
+import { SubItineraryComponent } from './sub-itinerary/sub-itinerary.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ChartComponent } from './chart/chart.component';
+
 import {
-  CovalentCommonModule,
-  CovalentDataTableModule,
-  CovalentDialogsModule,
-  CovalentExpansionPanelModule,
-  CovalentJsonFormatterModule,
-  CovalentLayoutModule,
-  CovalentLoadingModule,
-  CovalentMediaModule,
-  CovalentMenuModule,
-  CovalentMessageModule,
-  CovalentNotificationsModule,
-  CovalentPagingModule,
-  CovalentSearchModule,
-  CovalentStepsModule,
-  CovalentTabSelectModule
-} from "@covalent/core";
-import {ErrorComponent} from './error/error.component';
-import {AppRoutingModule} from "./app-routing.module";
-import {SubItineraryComponent} from './sub-itinerary/sub-itinerary.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {CredentialService} from "./service/credential.service";
-import {NavBarComponent} from './nav-bar/nav-bar.component';
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule
+} from '@angular/material';
+
+/* Covalent core modules */
 import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule
-} from "@angular/material";
-import {CommonModule} from "@angular/common";
-import {NgxChartsModule} from "@swimlane/ngx-charts";
-import {ChartComponent} from './chart/chart.component';
+    CovalentCommonModule,
+    CovalentDataTableModule,
+    CovalentDialogsModule,
+    CovalentExpansionPanelModule,
+    CovalentJsonFormatterModule,
+    CovalentLayoutModule,
+    CovalentLoadingModule,
+    CovalentMediaModule,
+    CovalentMenuModule,
+    CovalentMessageModule,
+    CovalentNotificationsModule,
+    CovalentPagingModule,
+    CovalentSearchModule,
+    CovalentStepsModule,
+    CovalentTabSelectModule
+} from '@covalent/core';
+// (optional) Additional Covalent Modules imports
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentHttpModule } from '@covalent/http';
+
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UnitsComponent,
-    ViewComponent,
-    FileComponent,
-    FilesComponent,
-    LoginComponent,
-    SignUpComponent,
-    ItineraryComponent,
-    UnitComponent,
-    ErrorComponent,
-    SubItineraryComponent,
-    NavBarComponent,
-    ChartComponent
-  ],
-  imports: [
-    BrowserModule,
-    CommonModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
+    declarations: [
+        AppComponent,
+        UnitsComponent,
+        ViewComponent,
+        FileComponent,
+        FilesComponent,
+        LoginComponent,
+        SignUpComponent,
+        ItineraryComponent,
+        UnitComponent,
+        ErrorComponent,
+        SubItineraryComponent,
+        NavBarComponent,
+        ChartComponent
+    ],
+    imports: [
+        BrowserModule,
+        CommonModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
 
-    AppRoutingModule,
+        AppRoutingModule,
 
-    /** Material Modules */
-    MatButtonModule,
-    MatListModule,
-    MatIconModule,
-    MatCardModule,
-    MatMenuModule,
-    MatInputModule,
-    MatButtonToggleModule,
-    MatProgressSpinnerModule,
-    MatSelectModule,
-    MatSlideToggleModule,
-    MatDialogModule,
-    MatSnackBarModule,
-    MatToolbarModule,
-    MatTabsModule,
-    MatSidenavModule,
-    MatTooltipModule,
-    MatRippleModule,
-    MatRadioModule,
-    MatGridListModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSliderModule,
-    MatAutocompleteModule,
-    MatNativeDateModule,
+        /** Material Modules */
+        MatButtonModule,
+        MatListModule,
+        MatIconModule,
+        MatCardModule,
+        MatMenuModule,
+        MatInputModule,
+        MatButtonToggleModule,
+        MatProgressSpinnerModule,
+        MatSelectModule,
+        MatSlideToggleModule,
+        MatDialogModule,
+        MatSnackBarModule,
+        MatToolbarModule,
+        MatTabsModule,
+        MatSidenavModule,
+        MatTooltipModule,
+        MatRippleModule,
+        MatRadioModule,
+        MatGridListModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatSliderModule,
+        MatAutocompleteModule,
+        MatNativeDateModule,
 
-    /** Covalent Modules */
-    CovalentCommonModule,
-    CovalentLayoutModule,
-    CovalentMediaModule,
-    CovalentExpansionPanelModule,
-    CovalentStepsModule,
-    CovalentDialogsModule,
-    CovalentLoadingModule,
-    CovalentSearchModule,
-    CovalentPagingModule,
-    CovalentNotificationsModule,
-    CovalentMenuModule,
-    CovalentDataTableModule,
-    CovalentMessageModule,
-    // (optional) Additional Covalent Modules imports
-    CovalentHttpModule.forRoot(),
-    CovalentHighlightModule,
-    CovalentMarkdownModule,
-    CovalentDynamicFormsModule,
-    CovalentJsonFormatterModule,
-    CovalentDialogsModule,
-    CovalentTabSelectModule,
-    CovalentSearchModule,
-    /** Additional **/
-    NgxChartsModule,
-  ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptor,
-    multi: true,
-    deps: [CredentialService]
-  }],
-  bootstrap: [AppComponent]
+        /** Covalent Modules */
+        CovalentCommonModule,
+        CovalentLayoutModule,
+        CovalentMediaModule,
+        CovalentExpansionPanelModule,
+        CovalentStepsModule,
+        CovalentDialogsModule,
+        CovalentLoadingModule,
+        CovalentSearchModule,
+        CovalentPagingModule,
+        CovalentNotificationsModule,
+        CovalentMenuModule,
+        CovalentDataTableModule,
+        CovalentMessageModule,
+        // (optional) Additional Covalent Modules imports
+        CovalentHttpModule.forRoot(),
+        CovalentHighlightModule,
+        CovalentMarkdownModule,
+        CovalentDynamicFormsModule,
+        CovalentJsonFormatterModule,
+        CovalentDialogsModule,
+        CovalentTabSelectModule,
+        CovalentSearchModule,
+        /** Additional **/
+        NgxChartsModule,
+    ],
+    providers: [{
+        provide: HTTP_INTERCEPTORS,
+        useClass: AuthInterceptor,
+        multi: true,
+        deps: [CredentialService]
+    }],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
