0492fda71a3a36dac535230241cb084212e04cac
imRCS <43384985+imRCS@users.noreply.github.com>
2019-04-05 05:33:59 -0700

    itinerary base layout

--- webapp/src/app/sub-itinerary/sub-itinerary.component.ts
+++ webapp/src/app/sub-itinerary/sub-itinerary.component.ts
@@ -10,6 +10,12 @@ export class SubItineraryComponent implements OnInit {
     @Input()
     itinerary: Itinerary;
 
+  units = [
+    {text: 'Javascript', atribute2: 1 },
+    {text: 'CERN', atribute2: 1},
+  ];
+
+
     constructor() { }
 
     ngOnInit() {
