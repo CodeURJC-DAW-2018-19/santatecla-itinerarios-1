a57e3ec91f348f93cccb64a90411e39c7d49ac4b
daolao <dalao1002@gmail.com>
2019-04-07 23:05:22 +0200

    merge

--- webapp/src/app/units/units.component.ts
+++ webapp/src/app/units/units.component.ts
@@ -1,122 +1,121 @@
-import {Component, EventEmitter, Inject, Input, OnInit, Output} from '@angular/core';
-import {Unit} from '../model/unit';
-import {ResourcesService} from '../service/resources.service';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material';
-import {FileDialogComponent} from "../files/file.dialog.component";
-import {Itinerary} from "../model/itinerary";
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Itinerary } from "../model/itinerary";
+import { Unit } from '../model/unit';
+import { ResourcesService } from '../service/resources.service';
 
 export interface UnitDialogData {
-  unit: Unit;
-  callback: (Unit) => void;
+    unit: Unit;
+    callback: (Unit) => void;
 }
 
 export interface ItineraryDialogData {
-  itinerary: Itinerary;
-  callback: (Itinerary) => void;
-  unit: number
+    itinerary: Itinerary;
+    callback: (Itinerary) => void;
+    unit: number
 }
 
 @Component({
-  selector: 'app-units',
-  templateUrl: './units.component.html',
-  styleUrls: ['./units.component.scss']
+    selector: 'app-units',
+    templateUrl: './units.component.html',
+    styleUrls: ['./units.component.scss']
 })
 export class UnitsComponent implements OnInit {
-  units: Unit[];
-  searchInputTerm: string;
-  unit: number;
-  itinerary: Itinerary;
+    units: Unit[];
+    searchInputTerm: string;
+    unit: number;
+    itinerary: Itinerary;
 
-  constructor(private rest: ResourcesService, public dialog: MatDialog) {
-  }
+    constructor(private rest: ResourcesService, public dialog: MatDialog) {
+    }
 
-  ngOnInit(): void {
-    this.rest.fetchUnits().subscribe(units => this.units = units);
-  }
+    ngOnInit(): void {
+        this.rest.fetchUnits().subscribe(units => this.units = units);
+    }
 
-  onBlurEvent() {
+    onBlurEvent() {
 
-  }
+    }
 
-  createUnit(): void {
-    const dialogRef = this.dialog.open(AddUnitDialog, {
-      data: {
-        unit: this.unit,
-        callback: unit => this.units.push(unit)
-      }
-    });
+    createUnit(): void {
+        const dialogRef = this.dialog.open(AddUnitDialog, {
+            data: {
+                unit: this.unit,
+                callback: unit => this.units.push(unit)
+            }
+        });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('The dialog was closed');
+        });
+    }
 
-  deleteUnit(unit: Unit) {
-    this.rest.deleteResource(unit).subscribe(() => this.units = this.units.filter(e => e.id !== unit.id));
-  }
+    deleteUnit(unit: Unit) {
+        this.rest.deleteResource(unit).subscribe(() => this.units = this.units.filter(e => e.id !== unit.id));
+    }
 
-  addItinerary(unit): void {
-    const dialogRef = this.dialog.open(AddItineraryDialog, {
-      data: {
-        itinerary: this.itinerary,
-        unit: unit.id
-      }
-    });
+    addItinerary(unit): void {
+        const dialogRef = this.dialog.open(AddItineraryDialog, {
+            data: {
+                itinerary: this.itinerary,
+                unit: unit.id
+            }
+        });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('The dialog was closed');
+        });
+    }
 }
 
 @Component({
-  selector: 'app-add-unit',
-  templateUrl: './add-unit.component.html',
+    selector: 'app-add-unit',
+    templateUrl: './add-unit.component.html',
 })
 export class AddUnitDialog {
-  constructor(
-    public dialogRef: MatDialogRef<AddUnitDialog>,
-    private rest: ResourcesService,
-    @Inject(MAT_DIALOG_DATA) public data: UnitDialogData) {
-    if (!data.unit) {
-      data.unit = new Unit({}, this.rest)
+    constructor(
+        public dialogRef: MatDialogRef<AddUnitDialog>,
+        private rest: ResourcesService,
+        @Inject(MAT_DIALOG_DATA) public data: UnitDialogData) {
+        if (!data.unit) {
+            data.unit = new Unit({}, this.rest)
+        }
     }
-  }
 
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
 
-  addUnit() {
-    this.rest.saveUnit(this.data.unit).subscribe(unit => this.data.callback(unit));
-    this.dialogRef.close();
-  }
+    addUnit() {
+        this.rest.saveUnit(this.data.unit).subscribe(unit => this.data.callback(unit));
+        this.dialogRef.close();
+    }
 }
 
 @Component({
-  selector: 'app-add-itinerary',
-  templateUrl: './add-itinerary.component.html',
+    selector: 'app-add-itinerary',
+    templateUrl: './add-itinerary.component.html',
 })
 export class AddItineraryDialog {
-  constructor(
-    public dialogRef: MatDialogRef<AddItineraryDialog>,
-    private rest: ResourcesService,
-    @Inject(MAT_DIALOG_DATA) public data: ItineraryDialogData) {
-    if (!data.itinerary) {
-      data.itinerary = new Itinerary({}, this.rest)
+    constructor(
+        public dialogRef: MatDialogRef<AddItineraryDialog>,
+        private rest: ResourcesService,
+        @Inject(MAT_DIALOG_DATA) public data: ItineraryDialogData) {
+        if (!data.itinerary) {
+            data.itinerary = new Itinerary({}, this.rest)
+        }
     }
-  }
 
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
 
-  addItinerary() {
-    this.rest.fetchUnit(this.data.unit).subscribe(unit => {
-      this.rest.addResourceRelation(unit, this.data.itinerary, "itineraries").subscribe(itinerary => this.data.callback(itinerary))
-    });
-    this.dialogRef.close();
-  }
+    addItinerary() {
+        this.rest.fetchUnit(this.data.unit).subscribe(unit => {
+            this.rest.addResourceRelation(unit, this.data.itinerary, "itineraries").subscribe(itinerary => this.data.callback(itinerary))
+        });
+        this.dialogRef.close();
+    }
 }
