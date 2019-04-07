9b6cf412cc4a7c99a82f205b78950cfcd8fe0594
ziyao <ziyaozhan1998@gmail.com>
2019-04-07 18:00:58 +0200

    add unit

--- webapp/src/app/units/units.component.ts
+++ webapp/src/app/units/units.component.ts
@@ -54,7 +54,7 @@ export class AddUnitDialog {
     public dialogRef: MatDialogRef<AddUnitDialog>,
     private rest: ResourcesService,
     @Inject(MAT_DIALOG_DATA) public data: UnitDialogData) {
-    if(data.unit){
+    if(!data.unit){
       data.unit=new Unit({},this.rest)
     }
   }
